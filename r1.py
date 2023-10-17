e2fc18b5b1f5abd80a18914205ed418125017de0
ALEXANDER310592 <graxagg26@gmail.com>
2023-10-17 18:56:37 -0500

    que funcione

--- r1.py
+++ r1.py
@@ -4,7 +4,3 @@ para así poder automatizar el mundo a mi manera, según mis ideas.
 necesito mas herramientas espero encotnrarlas aquí y poder avanzar muy pronto y con mucha eficiencia 
 la espera me desespera de querer hacer muchas cosas, comerme al mundo con todas las ideas que tengo pero 
 muchas veces nose como comenzar, teniendo la idea, la organización y la estructura de como funciona
-peor no para el mundo que lo desea imaginarse sino verlo y por eso no arriesgan ni siquera 
-poquito del cual me considero tener pero por desgracia no poder explicarlo o darlo entender 
-con esa intensidad que mucha gente haciendo cosas insignificantes mueven y atraen mucha atención 
-de los demas. 
